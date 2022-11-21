5dcbb2ea922308cc8c3cefecf24055a23486a49c
KamienKaze <HygrekRazuuk@gmail.com>
2022-11-21 21:15:37 +0100

    new: whole json parsing from url

--- app/src/main/java/com/example/cocktailsoftheworld/MainActivity.java
+++ app/src/main/java/com/example/cocktailsoftheworld/MainActivity.java
@@ -1,20 +1,102 @@
 package com.example.cocktailsoftheworld;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.TextViewCompat;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<JSONObject> jsonObjects = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Hide action bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
+        new getDrinks().start();
+    }
 
+    class getDrinks extends Thread {
+
+        @Override
+        public void run() {
+
+            char startingLetter = 'a';
+
+            while(startingLetter <= 'z') {
+
+                try {
+                    URL url = new URL("https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + startingLetter);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    int cp;
+                    while ((cp = bufferedReader.read()) != -1) {
+                        stringBuilder.append((char) cp);
+
+                    }
+
+                    JSONObject jsonObject = new JSONObject(stringBuilder.toString());
+                    jsonObjects.add(jsonObject);
+
+                    httpURLConnection.disconnect();
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                startingLetter++;
+            }
+
+            for(int i = 0; i < jsonObjects.size(); i++) {
+                try {
+                    JSONObject jsonObject = jsonObjects.get(i);
+
+                    if(jsonObject.getString("drinks") != "null") {
+                        JSONArray jsonArray = jsonObject.getJSONArray("drinks");
+
+                        for (int j = 0; j < jsonArray.length(); j++) {
+
+                            jsonObject = jsonArray.getJSONObject(j);
+                            System.out.println(jsonObject.getString("idDrink"));
+
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
